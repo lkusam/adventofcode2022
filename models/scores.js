53a694e751a0e261498a6178573105a398d970c2
lkusam <lkusam@kent.edu>
2022-12-05 02:00:25 -0500

    -- Day2 Complete

--- models/scores.js
+++ models/scores.js
@@ -1,18 +1,47 @@
 class scores {
   constructor(oppScore, myScore) {
     this.oppSel = oppScore;
-    this.mySel = myScore;
-
+    this.mySel = this.getMyMove(myScore);
     this.oppScore = this.getScore(oppScore);
-    this.myScore = this.getScore(myScore);
+    this.myScore = this.getScore(this.mySel);
     this.winScore = this.myScore - this.oppScore;
     this.result = this.getResult();
     console.log(`Opp :${this.oppSel} - ${this.oppScore}. MyScore : ${this.mySel} - ${this.myScore}. WinScore : ${this.winScore}clear Current round result : ${this.result}`);
   }
+  getMyMove(givenMove) {
+    if (givenMove == "X") {
+      switch (this.oppSel) {
+        case "A":
+          return "Z";
+        case "B":
+          return "X";
+        case "C":
+          return "Y";
+      }
+    } else if (givenMove == "Y") {
+      switch (this.oppSel) {
+        case "A":
+          return "X";
+        case "B":
+          return "Y";
+        case "C":
+          return "Z";
+      }
+    } else {
+      switch (this.oppSel) {
+        case "A":
+          return "Y";
+        case "B":
+          return "Z";
+        case "C":
+          return "A";
+      }
+    }
+  }
   getResult() {
-    if (this.myScore > this.oppScore) {
+    if (this.winScore == 1 || this.winScore == -2) {
       return 6 + this.myScore;
-    } else if (this.myScore == this.oppScore) {
+    } else if (this.winScore == 0) {
       return 3 + this.myScore;
     } else {
       return this.myScore;
